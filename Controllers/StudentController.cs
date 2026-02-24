3c12381b1c098f5345d75cb694c1a33969f62480
WAGHMARE TANNU <tannuwaghmare15@gmail.com>
2026-02-24 12:46:23 +0530

    add is done

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using workshop.DBFOLDER;
+using workshop.Models;
+
+namespace workshop.Controllers
+    {
+    public class StudentController : Controller
+        {
+        private STUDENTDB db;
+
+        public StudentController(STUDENTDB s)
+            {
+            db = s;
+            }
+        [HttpGet]
+        public IActionResult add()
+            {
+            return View();
+            }
+        [HttpPost]
+        public IActionResult add(StudentModel s)
+            {
+            if (ModelState.IsValid)
+                {
+                db.students.Add(s);
+                db.SaveChanges();
+
+                return RedirectToAction("Add");
+
+                }
+            return View();
+            }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+            {
+            var students = await db.students.ToListAsync();
+
+            return View(students);
+
+            }
+        }
+    }
