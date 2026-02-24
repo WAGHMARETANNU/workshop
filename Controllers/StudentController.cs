1603eb4dd8e01d9fac974a0a23f21fd155d970e3
WAGHMARE TANNU <tannuwaghmare15@gmail.com>
2026-02-24 13:13:13 +0530

    insert , update , delete done

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -35,9 +35,65 @@ namespace workshop.Controllers
         public async Task<IActionResult> Create()
             {
             var students = await db.students.ToListAsync();
-
             return View(students);
 
+            }
+        
+        public IActionResult Edit(int id)
+            {
+            var student = db.students.Find(id);
+
+            if (student == null)
+                {
+                return NotFound();
+                }
+
+            return View(student);
+            }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(StudentModel s)
+            {
+            if (ModelState.IsValid)
+                {
+                db.students.Update(s);
+                db.SaveChanges();
+                //return RedirectToAction("Index");
+                return RedirectToAction("Edit", new { id = s.student_id });
+                }
+
+            return View(s);
+            }
+
+        // GET: Student/Delete/5
+        public IActionResult Delete(int id)
+            {
+            var student = db.students.Find(id);
+
+            if (student == null)
+                {
+                return NotFound();
+                }
+
+            return View(student);
+            }
+
+        // POST: Student/DeleteConfirmed/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+            {
+            var student = db.students.Find(id);
+
+            if (student != null)
+                {
+                db.students.Remove(student);
+                db.SaveChanges();
+                }
+
+            return RedirectToAction("Index");
             }
         }
     }
