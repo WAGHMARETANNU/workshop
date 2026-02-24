86305ac03315d766a55b8a511cc194aea48d711f
WAGHMARE TANNU <tannuwaghmare15@gmail.com>
2026-02-24 10:32:46 +0530

    till now

---
+++ DBFOLDER/STUDENTDB.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace workshop.DBFOLDER
+    {
+    public class STUDENTDB:DbContext
+        {
+        public STUDENTDB(DbContextOptions<STUDENTDB> options):base(options)
+            {
+            }
+
+        public DbSet<Models.StudentModel> students { get; set; }
+
+
+        }
+    }
